2d7de86f26bdc4d51a4d4ca4529a20cca1c39895
Venkat Arigela <akilesharigela@gmail.com>
2020-10-13 03:04:08 -0600

    Add files via upload

--- create_instance2.py
+++ create_instance2.py
@@ -165,13 +165,7 @@ Check this URL: http://storage.googleapis.com/{}/output.png
 Once the image is uploaded press enter to delete the instance.
 """.format(bucket))
 
-    if wait:
-        input()
 
-    print('Deleting instance.')
-
-    operation = delete_instance(compute, project, zone, instance_name)
-    wait_for_operation(compute, project, zone, operation['name'])
 
 
 if __name__ == '__main__':
